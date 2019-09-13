40377b37c419b7f5c720520bc206cfa434bd557c
Karl Saehun Chung <nycom13@gmail.com>
2019-09-13 23:48:58 +0900

    Add initialize configuration script

---
+++ init.el
@@ -0,0 +1,18 @@
+;;; init.el --- Load the full configuration -*- lexical-binding: t -*-
+
+(add-to-list 'load-path (expand-file-name "config" user-emacs-directory))
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+
+;;----------------------------------------------------------------------------
+;; Adjust garbage collection thresholds during startup, and thereafter
+;;----------------------------------------------------------------------------
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+;;----------------------------------------------------------------------------
+;; Bootstrap config
+;;----------------------------------------------------------------------------
+(require 'config-init)
