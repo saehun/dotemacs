ed9eeaa2cbf0a7b6f4fd6d9520bd2c5c14658f0f
Karl Saehun Chung <nycom13@gmail.com>
2019-09-14 04:10:26 +0900

    Add recentf editorconfig

--- config/init-misc.el
+++ config/init-misc.el
@@ -1,3 +1,15 @@
+;;----------------------------------------------------------------------------
+;; recent file mode
+;;----------------------------------------------------------------------------
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
 
+
+;;----------------------------------------------------------------------------
+;; editorconfig
+;;----------------------------------------------------------------------------
+(when (maybe-require-package 'editorconfig)
+  (add-hook 'prog-mode-hook 'editorconfig-mode))
 
 (provide 'init-misc)
