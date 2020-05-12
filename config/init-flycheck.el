bae8c0c3890534a7cea2437f80070a376f0dc63f
minidonut <nycom13@gmail.com>
2020-05-13 04:43:17 +0900

    Update flycheck

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -2,8 +2,24 @@
 ;;; Commentary:
 ;;; Code:
 
+
 (when (maybe-require-package 'flycheck)
   (add-hook 'after-init-hook 'global-flycheck-mode)
+
+  ;; https://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+  (defun my/use-eslint-from-node-modules ()
+    (let* ((root (locate-dominating-file
+                  (or (buffer-file-name) default-directory)
+                  "node_modules"))
+          (eslint
+            (and root
+                (expand-file-name "node_modules/.bin/eslint"
+                                  root))))
+      (when (and eslint (file-executable-p eslint))
+        (setq-local flycheck-javascript-eslint-executable eslint))))
+
+  (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
 
   (when (maybe-require-package 'flycheck-color-mode-line)
