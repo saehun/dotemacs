f80c4a3fee2e706256b2ddd6e651f35ebf9ec135
minidonut <nycom13@gmail.com>
2020-08-22 04:03:49 +0900

    add commit-current-file magit

--- config/init-magit.el
+++ config/init-magit.el
@@ -5,11 +5,18 @@
 ;;; Magit
 
 (when (maybe-require-package 'magit)
-  (setq magit-display-buffer-function
+  (setq 'magit-display-buffer-function
     (lambda (buffer)
       (display-buffer buffer '(display-buffer-same-window))))
 
+  (defun magit-commit-current-file ()
+    (interactive)
+    (progn
+      (magit-stage-file (buffer-file-name))
+      (magit-commit-create)))
+
   (setq magit-git-global-arguments '("--no-pager" "-c" "core.preloadindex=true" "-c" "log.showSignature=false" "-c" "color.ui=false" "-c" "color.diff=false")))
+
 
 (provide 'init-magit)
 ;;; init-magit.el ends here
