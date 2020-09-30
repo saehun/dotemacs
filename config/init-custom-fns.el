8ec53d7a2c731285dbf078cb8450917aa8c5af88
minidonut <nycom13@gmail.com>
2020-09-30 14:04:36 +0900

    add custom function to copy file path

--- config/init-custom-fns.el
+++ config/init-custom-fns.el
@@ -16,5 +16,15 @@
       (dired-up-directory)
       (dired (file-name-directory buffer-file-name))))
 
+(defun copy-file-name-to-clipboard ()
+  "Copy the current buffer file name to the clipboard."
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (kill-new filename)
+      (message "Copied buffer file name '%s' to the clipboard." filename))))
+
 (provide 'init-custom-fns)
 ;;; init-custom-fns.el ends here
