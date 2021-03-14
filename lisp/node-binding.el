296ca461764ddb3e30f2d877361983436c9f8e59
minidonut <nycom13@gmail.com>
2021-03-15 02:29:02 +0900

    Update js-snippet

--- lisp/node-binding.el
+++ lisp/node-binding.el
@@ -4,6 +4,19 @@
 
 (require 'cl-lib)
 
+(defun choose-directory (directory-to-start-in)
+  "Return a directory chosen by the user.  The user will be prompted
+to choose a directory starting with `directory-to-start-in'"
+  (let* ((ivy-read-prompt "Directory: ")
+         (counsel--find-file-predicate #'file-directory-p)
+         (default-directory directory-to-start-in)
+         (selected-directory
+          (ivy-read
+           ivy-read-prompt
+           #'read-file-name-internal
+           :matcher #'counsel--find-file-matcher)))
+    selected-directory))
+
 (cl-defun post-message-node (command &optional (data ""))
   "Send message."
   (eval-string
@@ -21,16 +34,20 @@
   "Evaluate elisp code stored in a string."
   (eval (car (read-from-string string))))
 
+
 (defun node-service-env ()
   "Execute `emacs-node' nodejs command and evaluate it's standard output."
   (let ((directory default-directory)
-         (filename (buffer-name))
+         (filename (if (buffer-file-name)
+                     (file-name-nondirectory (buffer-file-name))
+                     ""))
          (buffer (buffer-string))
          (region (if (region-active-p) ;; region
                     (filter-buffer-substring (region-beginning) (region-end))
                     "")))
     (json-encode
       (list
+        (cons "mode" (format "%s" major-mode))
         (cons "directory" directory)
         (cons "cursor" (list
           (cons "pos" (point))
@@ -44,12 +61,17 @@
 (defun node/counsel-command ()
   "List node command."
   (interactive)
-  (post-message-node-with-env "list"))
+  (post-message-node "list"))
 
 (defun node/counsel-open ()
   "List open."
   (interactive)
-  (post-message-node "open"))
+  (post-message-node "open-url"))
+
+(defun node/ts-unit-test ()
+  "Create and open test file."
+  (interactive)
+  (post-message-node-with-env "ts-unit-test"))
 
 (provide 'node-binding)
 ;;; node-binding.el ends here
