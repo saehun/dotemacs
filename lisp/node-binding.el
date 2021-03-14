7757e50b5d79d5d5568219bbc9db9ca7192b13a9
minidonut <nycom13@gmail.com>
2021-03-15 00:38:20 +0900

    Update node-binding

--- lisp/node-binding.el
+++ lisp/node-binding.el
@@ -3,58 +3,53 @@
 ;;; Code:
 
 (require 'cl-lib)
-(require 'request)
 
-(defun websocket-client-with-remote-server ()
-  ;; Emacs previous to Emacs 24 cannot handle wss.
-  ;; echo.websocket.org has an untrusted certificate, for the test to
-  ;; proceed, we need to disable trust checking.
-  (interactive)
-  (request
-    "http://localhost:10684"
-    :type "POST"
-    :data (json-encode '(("key" . "value") ("key2" . "value2")))
-    :headers '(("Content-Type" . "application/json"))
-    :success (cl-function
-              (lambda (&key data &allow-other-keys)
-                (message data)))))
+(cl-defun post-message-node (command &optional (data ""))
+  "Send message."
+  (eval-string
+    (with-output-to-string
+      (call-process "emacs-node" nil standard-output nil command data))))
+
+(cl-defun post-message-node-with-env (command &optional (data ""))
+  "Send message with env."
+  (if (> (buffer-size) 100000) (message "Buffer is to large: %d charaters" (buffer-size))
+  (eval-string
+    (with-output-to-string
+      (call-process "emacs-node" nil standard-output nil command data (node-service-env))))))
 
 (defun eval-string (string)
   "Evaluate elisp code stored in a string."
   (eval (car (read-from-string string))))
 
-(defun emacs-node-exec (command)
+(defun node-service-env ()
   "Execute `emacs-node' nodejs command and evaluate it's standard output."
-  (if (> (buffer-size) 100000) (message "Buffer is to large: %d charaters" (buffer-size))
-  (let (
-         (directory default-directory)
+  (let ((directory default-directory)
          (filename (buffer-name))
-         (cursor (format
-                   "{ \"pos\": %d, \"col\": %d, \"row\": %d }"
-                   (point)
-                   (current-column)
-                   (line-number-at-pos)))
          (buffer (buffer-string))
          (region (if (region-active-p) ;; region
                     (filter-buffer-substring (region-beginning) (region-end))
-                    ""))
-         )
-  (eval-string
-    (with-output-to-string
-      (call-process "emacs-node" nil standard-output nil
-        command
-        directory
-        filename
-        cursor
-        buffer
-        region
-        ))
-    ))))
+                    "")))
+    (json-encode
+      (list
+        (cons "directory" directory)
+        (cons "cursor" (list
+          (cons "pos" (point))
+          (cons "col" (current-column))
+          (cons "row" (line-number-at-pos))))
+        (cons "filename" filename)
+        (cons "buffer" buffer)
+        (cons "region" region)))))
 
-(defun emacs-node ()
-  "Json to Yaml."
+;; Command
+(defun node/counsel-command ()
+  "List node command."
   (interactive)
-  (emacs-node-exec "hi"))
+  (post-message-node-with-env "list"))
+
+(defun node/counsel-open ()
+  "List open."
+  (interactive)
+  (post-message-node "open"))
 
 (provide 'node-binding)
 ;;; node-binding.el ends here
