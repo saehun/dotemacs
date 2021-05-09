007c634073fdc17dc7c52a469d40674f10f0ea5e
minidonut <nycom13@gmail.com>
2021-05-10 01:19:36 +0900

    Update snippet

--- config/init-yasnippet.el
+++ config/init-yasnippet.el
@@ -6,7 +6,10 @@
   (yas-global-mode 1)
   (setq yas-snippet-dirs
     '("~/.emacs.d/snippets"))
-  (when (maybe-require-package 'ivy-yasnippet)))
+
+  (when (maybe-require-package 'ivy-yasnippet))
+  (when (maybe-require-package 'string-inflection)
+    (string-inflection-all-cycle)))
 
 (provide 'init-yasnippet)
 ;;; init-yasnippet.el ends here
