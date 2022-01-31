bc508aac4d3cbdeb20c5f7e59bb4319c5b266187
minidonut <nycom13@gmail.com>
2022-01-31 11:44:03 +0900

    feat(utils): start empty buffer with `text-mode`

--- config/init-utils.el
+++ config/init-utils.el
@@ -153,11 +153,12 @@
 ;;----------------------------------------------------------------------------
 (defun new-empty-buffer ()
   "Open a new empty buffer.
-   URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'
+   URL `http://xahlee.info/emacs/emacs/emacs_new_empty_buffer.html'
    Version 2016-08-11"
   (interactive)
-  (let ((-buf (generate-new-buffer "untitled")))
-    (switch-to-buffer -buf)
+  (let (($buf (generate-new-buffer "untitled")))
+    (switch-to-buffer $buf)
+    (setq initial-major-mode 'text-mode)
     (funcall initial-major-mode)
     (setq buffer-offer-save t)))
 
