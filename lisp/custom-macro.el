0f9ce05bb4763e0a4704a61c90adb77f14e3edd9
minidonut <nycom13@gmail.com>
2022-04-12 03:39:14 +0900

    chore(lisp): revert buffer now message

--- lisp/custom-macro.el
+++ lisp/custom-macro.el
@@ -17,9 +17,9 @@
 
 (defun revert-buffer-no-confirm ()
   "Revert buffer without confirmation."
-  (interactive) (revert-buffer t t))
-(global-set-key (kbd "C-s-r")  'revert-buffer-no-confirm)
-
+  (interactive)
+  (revert-buffer t t)
+  (message "Buffer reverted ✨"))
 
 (defun open-todo ()
   "Open todo markdown file."
