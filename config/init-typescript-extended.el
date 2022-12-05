1846179e1e13742f354a2285c88611134a4f98f1
minidonut <nycom13@gmail.com>
2022-12-05 17:19:00 +0900

    feat(typescript): refactor and codefix now using ivy

--- config/init-typescript-extended.el
+++ config/init-typescript-extended.el
@@ -24,19 +24,52 @@
                          :description ,(plist-get refactor-action-info :description)))
                 (plist-get applicable-refactor-info :actions)))
             applicable-refactor-infos)))
-    (tide-select-refactor-ivy available-refactors #'tide-get-refactor-description)))
+    (tide-select-ivy "refactor" available-refactors #'tide-get-refactor-description)))
 
-(defun tide-select-refactor-ivy (candidates label-fn)
-  "Select refactor CANDIDATES with ivy buffer.
+(defun tide-select-ivy (title candidates label-fn)
+  "Select refactor CANDIDATES with ivy buffer and show TITLE.
 LABEL-FN to pick description"
   (let ((collection (make-hash-table :test 'equal)))
     (dolist (item candidates)
       (puthash (funcall label-fn item) item collection))
-    (let* ((selected-text (ivy-read "Select refactor: " (hash-table-keys collection)))
+    (let* ((selected-text (ivy-read (format "Select %s: " title) (hash-table-keys collection)))
             (selected-candidate (gethash selected-text collection)))
       (if (null selected-candidate)
-        (error (concat "No refactor selected: " selected-text))
+        (error (concat (format "No %s selected: " title) selected-text))
         selected-candidate))))
+
+(defun my-tide-fix (&optional arg)
+  "Apply code fix for the error at point.
+
+When invoked with a prefix arg, apply code fix for all the errors
+in the file that are similar to the error at point."
+  (interactive "P")
+  (if arg
+      (my-tide-code-fix #'my-tide-apply-codefix-for-all-in-file)
+    (my-tide-code-fix #'tide-apply-codefix)))
+
+(defun my-tide-code-fix (codefix-fn)
+  (unless (tide-get-flycheck-errors-ids-at-point)
+    (error "No errors available at current point."))
+  (let ((response (tide-command:getCodeFixes)))
+    (tide-on-response-success response
+      (let ((fixes (plist-get response :body)))
+        (my-tide-apply-codefixes fixes codefix-fn)))))
+
+(defun my-tide-apply-codefix-for-all-in-file (fix)
+  (tide-apply-codefix fix)
+  (-when-let* ((fix-id (plist-get fix :fixId))
+               (response (tide-command:getCombinedCodeFix fix-id)))
+    (tide-on-response-success response
+      (tide-apply-codefix (plist-get response :body)))))
+
+(defun my-tide-apply-codefixes (fixes codefix-fn)
+  (cond ((= 0 (length fixes)) (message "No code-fixes available."))
+        ((= 1 (length fixes)) (funcall codefix-fn (car fixes)))
+        (t (funcall
+            codefix-fn
+            (tide-select-ivy "fix" fixes #'tide-get-fix-description)))))
+
 
 (provide 'init-typescript-extended)
 ;;; init-typescript-extended.el ends here
