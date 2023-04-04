d4f72aac073cdcf86641949e165d4423d6291436
minidonut <nycom13@gmail.com>
2023-04-05 02:00:27 +0900

    feat: fix copilot node executable path

--- config/init-copilot.el
+++ config/init-copilot.el
@@ -4,7 +4,9 @@
 
 (use-package copilot
   :straight (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el"))
-  :ensure t)
+  :ensure t
+  :config
+  (setq copilot-node-executable "/usr/local/n/versions/node/16.17.1/bin/node"))
 
 (provide 'init-copilot)
 ;;; init-copilot.el ends here
